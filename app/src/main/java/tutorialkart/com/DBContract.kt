b84e2e3e9b9bb3e3ab48c99ce234511aa488a9d1
Nolan Turin <nolan@Nolans-MacBook-Air-4.local>
2020-04-16 23:43:20 -0700

    Initial commit

---
+++ app/src/main/java/tutorialkart/com/DBContract.kt
@@ -0,0 +1,14 @@
+package tutorialkart.com
+
+import android.provider.BaseColumns
+
+class DBContract {
+    class UserEntry : BaseColumns{
+        companion object{
+            val TABLE_NAME = "users"
+            val COLUMN_USER_ID = "userid"
+            val COLUMN_NAME = "name"
+            val COLUMN_AGE = "age"
+        }
+    }
+}
---
+++ app/src/main/java/tutorialkart/com/UserModel.kt
@@ -0,0 +1,3 @@
+package tutorialkart.com
+
+class UserModel (val userid: String, val name: String, val age: String)
